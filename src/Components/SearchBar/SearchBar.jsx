8d6c4e3ea12f61fb163bce29b60506a703e3ccbe
Justin Lau <justinlau094@hotmail.com>
2023-09-19 15:15:31 -0400

    async syntax

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -20,16 +20,18 @@ const SearchBar = ({ addCard }) => {
         clearTimeout(delay);
 
         if (e.target.value) {
-            delay = setTimeout(async () => {
-                try {
-                    const search = await axios.get(`${process.env.REACT_APP_URL}/cardSearch?name=${e.target.value}`)
-                    const searchResults = search.data.slice(0, 8)
-                    setInputList(searchResults);
-                    setIsLoading(false);
-                } catch (error) {
-                    console.error('Error fetching search results:', error);
-                    setIsLoading(false);
-                }
+            delay = setTimeout(() => {
+                (async () => {
+                    try {
+                        const search = await axios.get(`${process.env.REACT_APP_URL}/cardSearch?name=${e.target.value}`)
+                        const searchResults = search.data.slice(0, 8)
+                        setInputList(searchResults);
+                        setIsLoading(false);
+                    } catch (error) {
+                        console.error('Error fetching search results:', error);
+                        setIsLoading(false);
+                    }
+                })()
             }, 2000);
         }
     };
